005f064a6491cf26beae1833c3702130b261f2d8
Adedapo Ajuwon <dapseen@gmail.com>
2026-07-21 09:59:28 +0100

    fix: update readme and application performance

--- api/main.go
+++ api/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Cardmarket-Live!")
+		fmt.Fprintf(w, "Hello, Cardmarket! Here we go again!")
 	})
 	http.ListenAndServe(":8001", nil)
 }
